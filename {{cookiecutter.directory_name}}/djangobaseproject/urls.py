c6b4187ccc6de5a96a4fdf0711a6ec0713b74864
Stefan Szepe <szepe.stefan@gmail.com>
2022-06-16 12:28:13 +0200

    serve static and media files in debug mode; added debug-toolbar

--- {{cookiecutter.directory_name}}/djangobaseproject/urls.py
+++ {{cookiecutter.directory_name}}/djangobaseproject/urls.py
@@ -13,13 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webpage.urls', namespace='webpage')),
     path('infos/', include('infos.urls', namespace='infos'))
 ]
+
 handler404 = 'webpage.views.handler404'
+
+if settings.DEBUG:
+    import debug_toolbar
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.urls import get_resolver
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+
+
+    print(get_resolver().url_patterns)
