87f82b6408b989dca87cb0d08f2ad96faa375a36
Stefan Szepe <szepe.stefan@gmail.com>
2022-06-16 12:06:46 +0200

    updated settings in cookiecutter.json and metadata.py
    cookiecutter.json:
    - added
     - project_slug
     - project_short_description
     - project_group
     - version
     - language_code
     - timezone
     - theme
     - github_url
     - shared_url
    - appended
     - _copy_without_render
    
    THese properties allow the configuration of the django project. In addition to HTML files, also JavaScript and CSS files should just be copied by cookiecutter and not processed.
    
    metadata.py:
    - changed to use cookicutter variables:
     - description
     - github
     - version
    - changed text to meet mdw requirements:
     - author
     - matomo_url
     - social_media

--- {{cookiecutter.directory_name}}/webpage/metadata.py
+++ {{cookiecutter.directory_name}}/webpage/metadata.py
@@ -3,20 +3,20 @@
 
 PROJECT_METADATA = {
     'title': '{{cookiecutter.project_title}}',
-    'author': 'Peter Andorfer',
+    'author': 'Peter Andorfer, Stefan Szepe',
     'subtitle': 'A django project to bootstrap further web-app developments',
-    'description': 'A django project to bootstrap further web-app developments',
-    'github': 'https://github.com/acdh-oeaw/djangobaseproject',
+    'description': '{{cookiecutter.project_short_description}}',
+    'github': '{{cookiecutter.github_url}}',
     'purpose_de': 'der Schaffung einer einheitlichen\
     und generischen Grundlage für Web Applikationen.',
     'purpose_en': 'to bootstrap web development.',
-    'version': '0.0.1',
+    'version': '{{cookiecutter.version}}',
     'matomo_id': 'provide some',
-    'matomo_url': '//matomo.acdh.oeaw.ac.at/',
+    'matomo_url': '//mdw.ac.at/piwik/',
     'imprint': '/imprint',
     'social_media': [
-        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
-        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+        ('fab fa-twitter fa-2x', 'https://twitter.com/mdwwien'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/user/mdwvienna'),
     ],
     'app_type': 'database',  # database|website|service|tool|digital-edition
 }
