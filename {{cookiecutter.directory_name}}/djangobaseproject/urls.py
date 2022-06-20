f2cf08de6c0a2e9f3344637aae61f7927b60c35e
Stefan Szepe <szepe@mdw.ac.at>
2022-06-20 18:14:12 +0200

    added logoin and logout urls; added functionality to append urlpatterns automatically with local apps

--- {{cookiecutter.directory_name}}/djangobaseproject/urls.py
+++ {{cookiecutter.directory_name}}/djangobaseproject/urls.py
@@ -14,15 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import traceback
+
+from importlib import import_module
+from sys import stdout
+
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webpage.urls', namespace='webpage')),
-    path('infos/', include('infos.urls', namespace='infos'))
+    path('infos/', include('infos.urls', namespace='infos')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+# import urls from LOCAL_APPS as defines in base.py
+for app in settings.LOCAL_APPS:
+    try:
+        # get urls.py from the respective app
+        print(f"############## {app} ##############")
+        _module = import_module('%s.urls' % app)
+        if settings.DEBUG:
+            print(app)
+        urlpatterns += _module.urlpatterns
+    except Exception as error:
+        stdout.write(f"Error: {error}, {traceback.format_exc()}")
 
 handler404 = 'webpage.views.handler404'
 
@@ -36,6 +56,5 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
-
 
     print(get_resolver().url_patterns)
