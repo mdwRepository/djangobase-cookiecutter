f7471653320b65bb34f785fbca311d059c41992c
Stefan Szepe <szepe@mdw.ac.at>
2022-06-20 18:10:34 +0200

    added tou_enabled setting to cookiecutter.json; added Terms of Use model and admin interface (incl. migrations)

--- {{cookiecutter.directory_name}}/users/admin.py
+++ {{cookiecutter.directory_name}}/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(CustomGroup)
+admin.site.register(CustomUser)
+
+{% if cookiecutter.tou_enabled == 'y' -%}
+
+class TermsOfUseAcceptedAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', 'signed_at_time', 'tou_version')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+admin.site.register(TermsOfUse)
+admin.site.register(TermsOfUseAccepted, TermsOfUseAcceptedAdmin)
+
+{%- endif %}
--- {{cookiecutter.directory_name}}/users/models.py
+++ {{cookiecutter.directory_name}}/users/models.py
@@ -3,11 +3,18 @@
 import datetime
 import logging
 
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser, Group
 from django.utils.translation import gettext_lazy as _
 
+from guardian.models import UserObjectPermissionBase, GroupObjectPermissionBase
+
+from core.mixins import BaseEntityMetadataMixin, ExtIdMixin
 from .managers import CustomUserManager
 
 log = logging.getLogger(__name__)
@@ -41,6 +48,10 @@ class CustomUser(AbstractUser):
     )
     # e-mail confirmed? (after signup for external users; LDAP users have e-mail confirmed automatically set)
     email_confirmed = models.BooleanField(
+        default=False
+    )
+    # Terms of Use accepted?
+    current_tou_accepted = models.BooleanField(
         default=False
     )
     # date the user joined this application
@@ -84,7 +95,9 @@ def get_custom_anon_user(CustomUser):
     return CustomUser(
         username='AnonymousUser',
         birth_date=datetime.date(1410, 7, 15),
-        # current_tou_accepted=True,
+        {% if cookiecutter.enable_tou == 'y' -%}
+        current_tou_accepted=True,
+        {%- endif %}
     )
 
 
@@ -156,5 +169,113 @@ class CustomLDAPBackend(LDAPBackend):
 
     def username_matches_regex(self, username):
         return re.match(self.settings.USERNAME_REGEX, username)
+
+{%- endif %}
+
+
+# ---------------------------------------------------------------------------#
+# Terms Of Use                                                               #
+# ---------------------------------------------------------------------------#
+
+# Terms Of Use if activated
+{% if cookiecutter.tou_enabled == 'y' -%}
+
+class TermsOfUse(ExtIdMixin, BaseEntityMetadataMixin):
+    """
+    Terms of Use of the respective service
+     - multiple versions
+    """
+    content = models.TextField(
+        null=True,
+        blank=True
+    )
+    version = models.CharField(
+        max_length=25,
+        null=True,
+        blank=True,
+        default='1.0'
+    )
+    date = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    language = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        default='english',
+        help_text=_("Please provide the langauge of the Terms of Use document."),
+    )
+
+    class Meta:
+        verbose_name = _('Terms of Use')
+        verbose_name_plural = _('Terms of Use')
+        ordering = ['-created_at_time']
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('tou:details', args=[str(self.ext_id)])
+
+    def get_current_tou(self):
+        return TermsOfUse.objects.all().latest('date')
+
+
+# Guardian object level permissions for terms of use
+
+class TermsOfUseUserObjectPermission(UserObjectPermissionBase):
+    content_object = models.ForeignKey(TermsOfUse, on_delete=models.CASCADE)
+
+
+class TermsOfUseGroupObjectPermission(GroupObjectPermissionBase):
+    content_object = models.ForeignKey(TermsOfUse, on_delete=models.CASCADE)
+
+
+class TermsOfUseAccepted(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='user_tous',
+    )
+    signed_at_time = models.DateTimeField(
+        auto_now=True
+    )
+    tou_version = models.ForeignKey(
+        TermsOfUse,
+        related_name='user_tou_versions',
+        on_delete=models.DO_NOTHING,
+        null=True
+    )
+
+    class Meta:
+        ordering = ('user',)
+        unique_together = ('tou_version', 'user',)
+        verbose_name = _('Accepted TOU')
+        verbose_name_plural = _('Accepted TOUs')
+
+    def __str__(self):
+        return '%s accepted %s (%s) on %s' % (
+            self.user, self.tou_version.title, self.tou_version.version, self.signed_at_time)
+
+
+@receiver(post_save, sender=TermsOfUseAccepted)
+def update_user_tou_accepted(sender, instance, created, **kwargs):
+    if created:
+        cu = CustomUser.objects.get(pk=instance.user.id)
+        print(cu)
+        cu.current_tou_accepted = True
+        cu.save()
+
+
+@receiver(post_delete, sender=TermsOfUseAccepted)
+def remove_user_tou_accepted(sender, instance, **kwargs):
+    cu = CustomUser.objects.get(pk=instance.user.id)
+    print(cu)
+    cu.current_tou_accepted = False
+    cu.save()
 
 {%- endif %}
