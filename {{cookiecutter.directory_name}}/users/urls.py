e96ebb723adb8cec785c47ef11e1df46f4330c22
Stefan Szepe <szepe@mdw.ac.at>
2022-06-20 18:13:07 +0200

    added terms of use urls

---
+++ {{cookiecutter.directory_name}}/users/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+from django.urls import include, path
+
+from .views import TermsOfUseView, TermsOfUseAcceptView
+
+urlpatterns = [
+    path('terms-of-use/', TermsOfUseView.as_view(), name='terms_of_use'),
+    path('terms-of-use-accept/', TermsOfUseAcceptView.as_view(), name='terms_of_use_accept'),
+]
