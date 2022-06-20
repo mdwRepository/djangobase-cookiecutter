9750218cc09ea13c34ac1f059160c783b5ce6e6e
Stefan Szepe <szepe@mdw.ac.at>
2022-06-20 12:20:03 +0200

    added simple input filter for admin

---
+++ {{cookiecutter.directory_name}}/core/filters.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+
+from django.contrib.admin import SimpleListFilter
+
+
+class InputFilter(SimpleListFilter):
+    template = 'admin/input_filter.html'
+
+    def lookups(self, request, model_admin):
+        # Dummy, required to show the filter.
+        return ((),)
+
+    def choices(self, changelist):
+        # Grab only the "all" option.
+        all_choice = next(super().choices(changelist))
+        all_choice['query_parts'] = (
+            (k, v)
+            for k, v in changelist.get_filters_params().items()
+            if k != self.parameter_name
+        )
+        yield all_choice
