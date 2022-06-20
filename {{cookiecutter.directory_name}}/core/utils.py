68739d31dbce4bf87339c294817c972d599d762c
Stefan Szepe <szepe@mdw.ac.at>
2022-06-20 12:14:44 +0200

    added parse date and century from date functions to date / time helpers; source: https://github.com/acdh-oeaw/mmp/blob/master/archiv/utils.py

--- {{cookiecutter.directory_name}}/core/utils.py
+++ {{cookiecutter.directory_name}}/core/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import datetime
+import re
 
 
 # ---------------------------------------------------------------------------#
@@ -33,6 +34,33 @@ def current_month():
 
 def max_value_next_year():
     return int(current_year() + 1)
+
+
+def parse_date(input, default=1):
+    # https://github.com/acdh-oeaw/mmp/blob/master/archiv/utils.py
+    dates = (re.findall(r'\d+', input), input)[0]
+    try:
+        date_str = dates[0]
+    except IndexError:
+        date_str = default
+    try:
+        out = int(date_str)
+    except (ValueError, TypeError):
+        out = default
+    return out
+
+
+def cent_from_year(year):
+    """
+    takes an integer and returns the matching century
+    """
+    # https://github.com/acdh-oeaw/mmp/blob/master/archiv/utils.py
+    if year == 0:
+        return 1
+    elif year < 0:
+        return -(year - 1) // -100
+    else:
+        return (year - 1) // 100 + 1
 
 
 # obfuscate data
