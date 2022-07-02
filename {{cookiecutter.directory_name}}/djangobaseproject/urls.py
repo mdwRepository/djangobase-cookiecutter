617997b2f602a2b5c3c7a0ad56ee0e09af80ea1e
Stefan Szepe <szepe.stefan@gmail.com>
2022-07-02 12:44:54 +0200

    changed infos url handling to utilize import urls from LOCAL_APPS function

--- {{cookiecutter.directory_name}}/djangobaseproject/urls.py
+++ {{cookiecutter.directory_name}}/djangobaseproject/urls.py
@@ -49,7 +49,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('infos/', include('infos.urls', namespace='infos')),
     path('vocabs/', include('vocabs.urls', namespace='vocabs')),
     path('vocabs-ac/', include('vocabs.dal_urls', namespace='vocabs-ac')),
     path('', include('webpage.urls', namespace='webpage')),
