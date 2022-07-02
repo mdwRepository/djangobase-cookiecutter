0513f59af8fda877cf2c91662ea3550fd1d65012
Stefan Szepe <szepe.stefan@gmail.com>
2022-07-02 15:56:45 +0200

    changed user login route back to accounts/login, removed unused import custom_layout_object.py

--- {{cookiecutter.directory_name}}/users/user_urls.py
+++ {{cookiecutter.directory_name}}/users/user_urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken import views as token_view
 from .views import user_login, user_logout
 
 urlpatterns = [
-    path('login/', user_login, name='user_login'),
+    path('accounts/login/', user_login, name='user_login'),
     path('logout/', user_logout, name='user_logout'),
     path('api-token-auth/', token_view.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
